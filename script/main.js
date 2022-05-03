cb44e40ed1cab9c86fb67e661d94184b8c24c0ab
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2022-05-03 18:51:48 -0300

    chore: day 2 finished

---
+++ script/main.js
@@ -0,0 +1,18 @@
+function onScroll() {
+
+  if (scrollY === 0) {
+    navigation.classList.remove("scroll");
+  } else {
+    navigation.classList.add("scroll");
+  }
+
+}
+
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
