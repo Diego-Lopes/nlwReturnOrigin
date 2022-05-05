7fcacfa807a2c69ab8e5441ba77dae40ba2eeded
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2022-05-05 18:29:00 -0300

    refactore: modification structure project

--- script/main.js
+++ script/main.js
@@ -1,13 +1,27 @@
-function onScroll() {
+window.addEventListener('scroll', onScroll);
 
+onScroll();
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if (scrollY === 0) {
     navigation.classList.remove("scroll");
   } else {
     navigation.classList.add("scroll");
   }
-
 }
 
+function showBackToTopButtonOnScroll() {
+  if (scrollY <= 400) {
+    backToTopButton.classList.remove("show");
+  } else {
+    backToTopButton.classList.add("show");
+  }
+}
 
 function openMenu() {
   document.body.classList.add("menu-expanded");
